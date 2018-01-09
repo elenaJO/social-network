50362794a513c6faabf971714666b81b7e6d3e2d
lauraVelarde <penelope_rebeca_19@hotmail.com>
2018-01-09 15:37:03 -0500

    firebase

--- js/search.js
+++ js/search.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
   $('.button-collapse').sideNav();
+  /* firebase*/
+  var config = {
+    apiKey: 'AIzaSyBTyPXp0vll8d2Fvi5nViLsKntlNxapEFY',
+    authDomain: 'red-social-a1aeb.firebaseapp.com',
+    databaseURL: 'https://red-social-a1aeb.firebaseio.com',
+    projectId: 'red-social-a1aeb',
+    storageBucket: 'red-social-a1aeb.appspot.com',
+    messagingSenderId: '445743781768'
+  };
+  firebase.initializeApp(config);
+
+  $('#search').keyup(function() {
+    var name = $(this).val();
+    console.log(name);
+  });
 });
