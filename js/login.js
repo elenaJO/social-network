6a708ce8684d59bb523608b27931054cad4b6083
elenaJO <ele.lucero1993@gmail.com>
2018-01-09 00:29:28 -0500

    funcionalidad de seguidores

--- js/login.js
+++ js/login.js
@@ -19,17 +19,20 @@ $(document).ready(function() {
       // guardando la imagen y nombre;
       localStorage.photo = result.user.photoURL;
       localStorage.name = result.user.displayName;
-      console.log(result.user.photoURL);
+      localStorage.id = result.user.uid;
+      console.log(result.user);
       guardarFirebase(result.user);
     });
-    window.location.href = '../views/prueb.html';
+    // window.location.href = '../views/profile.html';
   });
   // funcion para guardar en firebase los datos de quien entra
   function guardarFirebase(user) {
     var usuario = {
       uid: user.uid,
       nombre: user.displayName,
-      foto: user.photoURL
+      foto: user.photoURL,
+      mail: user.email,
+      seguidores: 31,
     };
     firebase.database().ref('usuarios/' + user.uid).set(usuario);
   }
--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
   $('.button-collapse').sideNav();
+  $('#name-user').text(localStorage.name);
+  $('#foto').attr('src',localStorage.photo);
+  var $seguidores = $('#seguidores');
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyBTyPXp0vll8d2Fvi5nViLsKntlNxapEFY',
+    authDomain: 'red-social-a1aeb.firebaseapp.com',
+    databaseURL: 'https://red-social-a1aeb.firebaseio.com',
+    projectId: 'red-social-a1aeb',
+    storageBucket: 'red-social-a1aeb.appspot.com',
+    messagingSenderId: '445743781768'
+  };
+  console.log(localStorage.id);
+  firebase.initializeApp(config);
+  var dbRef = firebase.database().ref('usuarios');
+  var dbRefUsu = dbRef.child(localStorage.id);
+  dbRefUsu.on('value', function(snap) {
+    $seguidores.text((snap.val()['seguidores']));
+  });
 });
