5e26a318fb40457397341e4492c0e208503e2e67
alexandracch <alexa_theprinces@hotmail.com>
2018-01-09 15:35:23 -0500

    primeros archivos

---
+++ js/newsfeed.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+$(document).ready(function() {
+  //configuración a firebase
+  var config = {
+    apiKey: 'AIzaSyBTyPXp0vll8d2Fvi5nViLsKntlNxapEFY',
+    authDomain: 'red-social-a1aeb.firebaseapp.com',
+    databaseURL: 'https://red-social-a1aeb.firebaseio.com',
+    projectId: 'red-social-a1aeb',
+    storageBucket: 'red-social-a1aeb.appspot.com',
+    messagingSenderId: '445743781768'
+  };
+  firebase.initializeApp(config);
+
+  // 
+});
