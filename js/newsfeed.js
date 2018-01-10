4427ec7db4a5040913ab9ca47281dfb877a067e0
alexandracch <alexa_theprinces@hotmail.com>
2018-01-10 02:12:24 -0500

    funcionalidad de posteo

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,6 +1,6 @@
 // Initialize Firebase
 $(document).ready(function() {
-  //configuración a firebase
+  // configuración a firebase
   var config = {
     apiKey: 'AIzaSyBTyPXp0vll8d2Fvi5nViLsKntlNxapEFY',
     authDomain: 'red-social-a1aeb.firebaseapp.com',
@@ -10,6 +10,50 @@ $(document).ready(function() {
     messagingSenderId: '445743781768'
   };
   firebase.initializeApp(config);
+   // variables necesarias para funcionalidad
 
+  var $textArea = $('#textarea1');
+  var $postButton = $('#postButton');
+  var $date = moment().format('LT');
+
+  var $valueTextTarea = $textArea.val();
+  var $postButton2 = $('a[type=submit]');
+
+  // Evento de lectura de imagen 
+  $('#file-select').on('click', function(event) {
+    event.preventDefault();
+    $('#file').click();
+  });
+
+  $('input[type=file]').change(function() {
+    var file = (this.files[0].name).toString();
+
+    var reader = new FileReader();
+    $('#file-info').text('');
+    $('#file-info').text(file);
+    $postButton2.attr('disabled', false);
+
+    $postButton.on('click', function(event){
+      reader.onload = function(event) {
+        $('#preview img').attr('src', event.target.result);
+       
+      };
+      reader.readAsDataURL(this.files[0]);
+     });
+  });
+
+   
+
+  // habilitar funcion de postear (boton POST)
+  $textArea.on('keyup', function(event) {
+    // Si textArea no contiene nada o contiene un vacío el boton se desabilita
+    if ($(this).val().length === 0 || $(this).val().length === ' ') {
+      $postButton2.attr('disabled', true); // desabilita el boton
+      // textArea contiene algo
+    } else if ($(this).val().length >= 1) {
+      $postButton2.attr('disabled', false); // habilita el boton
+    }
+ });
 
 });
+
